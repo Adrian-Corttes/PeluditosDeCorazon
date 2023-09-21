428b2a00dab7e02d55e3842cd5279d1b0333767e
Adrian Cortes <ls-adrian@hotmail.com>
2023-09-20 22:28:38 -0500

    Modificacion del Slider

--- js/Swiper.js
+++ js/Swiper.js
@@ -2,10 +2,13 @@ var swiper = new Swiper(".mySwiper", {
    slidesPerView: 5,
    spaceBetween: 1,
    loop: true,
+   //Pagination
    pagination: {
      el: ".swiper-pagination",
      clickable: true,
+     dynamicBullets: true
    },
+   //Navigation
    navigation: {
      nextEl: ".swiper-button-next",
      prevEl: ".swiper-button-prev",
