8c0f4ac03c8f9f156b0586ea4ed8896c2c49a049
Adrian Cortes <ls-adrian@hotmail.com>
2023-09-21 20:49:31 -0500

    Slider finalizado - Swiper API

--- js/Swiper.js
+++ js/Swiper.js
@@ -1,16 +1,31 @@
-var swiper = new Swiper(".mySwiper", {
-   slidesPerView: 5,
-   spaceBetween: 1,
-   loop: true,
-   //Pagination
-   pagination: {
-     el: ".swiper-pagination",
-     clickable: true,
-     dynamicBullets: true
-   },
-   //Navigation
-   navigation: {
-     nextEl: ".swiper-button-next",
-     prevEl: ".swiper-button-prev",
-   },
- });
+var swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  slidesPerView: 4,
+  spaceBetween: 0,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  //Pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true
+  },
+  //Navigation
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints:{
+    0:{
+      slidesPerView: 1,
+    },
+    520:{
+      slidesPerView: 3,
+    },
+    950:{
+      slidesPerView: 5,
+    }
+  }
+});
